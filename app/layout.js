be6d42146197f1828f606692dabf8469b8854d86
Shivam kundu <kundushivam38@gmail.com>
2026-03-22 19:42:33 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "AI Career Coach",
   description: "",
+  icons: {
+    icon: "/logo.png", // make sure this exists in /public
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -21,10 +24,7 @@ export default function RootLayout({ children }) {
       }}
     >
       <html lang="en" suppressHydrationWarning>
-        <head>
-          <link rel="icon" href="/logo.png" sizes="any" />
-        </head>
-        <body className={`${inter.className}`}>
+        <body className={inter.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by Shivam kundu</p>
+                <p>Made with 💗 by Shivam Kundu</p>
               </div>
             </footer>
           </ThemeProvider>

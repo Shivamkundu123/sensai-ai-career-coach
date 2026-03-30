c22fe0c50b3551362b92316e18e4adb2273df79b
Shivam kundu <kundushivam38@gmail.com>
2026-03-31 00:42:51 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,15 +8,17 @@ const isProtectedRoute = createRouteMatcher([
   "/onboarding(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
+  // If the route is protected, auth.protect() will 
+  // automatically redirect unauthenticated users to sign-in.
   if (isProtectedRoute(req)) {
-    return auth().protect();
+    await auth.protect();
   }
-}); 
+});
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };

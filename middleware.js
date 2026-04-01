a0d704626ff8aa19b07fc5f6a10e02c2907a2a39
Shivam kundu <kundushivam38@gmail.com>
2026-04-01 21:06:07 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -12,7 +12,7 @@ export default clerkMiddleware(async (auth, req) => {
   // If the route is protected, auth.protect() will 
   // automatically redirect unauthenticated users to sign-in.
   if (isProtectedRoute(req)) {
-   return NextResponse.next();
+    await auth.protect();
   }
 });
 
